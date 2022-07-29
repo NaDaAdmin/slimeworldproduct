8a3600648a73dc9f601f3705d4dd3aa212b1f858
NaDaAdmin <client@nadadigital.co.kr>
2022-07-29 11:27:22 +0900

    시간 오래걸려서 receive token 영수증 부분 삭제

--- app/hashgraph/client.js
+++ app/hashgraph/client.js
@@ -355,12 +355,6 @@ class HashgraphClient extends HashgraphClientContract {
 		//Sign with the client operator private key and submit to a Hedera network
 		const txResponse = await signTx.execute(client);
 
-		const receipt = await txResponse.getReceipt(client)
-		
-		if (receipt.status.toString() !== "SUCCESS") {
-			return false;
-		}
-
 		let balance = await new AccountBalanceQuery()
 			.setAccountId(sender_id)
 			.execute(client)
